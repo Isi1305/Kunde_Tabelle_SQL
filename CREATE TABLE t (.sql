89df4e26e5f376ad728379ee96f61b99b1137ec8
Isi1305 <isabell.sophieandrea@gmail.com>
2026-05-18 12:02:46 +0200

    automatische Typumwandlung

--- CREATE TABLE t (.sql
+++ CREATE TABLE t (.sql
@@ -1,5 +1,12 @@
+--automatische Typumwandlung
+
+--Anlegen der Tabelle t
 CREATE TABLE t (
     i INTEGER, d DECIMAL(5,2), f FLOAT, c VARCHAR(3),
     b BOOLEAN
 );
+
+--Einfügen von zwei Datensätzden
+INSERT INTO t VALUES (1, 123.45, 78.46723, 'abc', TRUE);
+INSERT INTO t VALUES (1.5, 12345.678, 78.46723456789, 'abcdefghij', 23);
 
